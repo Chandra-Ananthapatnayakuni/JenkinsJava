1a1062eb31b8fb07d25b6cd5f611f95e76982eab
Hari Babu <nharibabu@csc.com>
2017-07-12 17:46:35 +0530

    Added the code, first commit

---
+++ src/main/java/com/csc/App.java
@@ -0,0 +1,24 @@
+package com.csc;
+
+import java.util.UUID;
+
+/**
+ * Generate a unique number
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        App obj = new App();
+        System.out.println("Hello World, Unique ID : " + obj.generateUniqueKey());
+    }
+    
+    public String generateUniqueKey(){
+    	
+    	String id = UUID.randomUUID().toString();
+    	return id;
+    	
+    }
+}
